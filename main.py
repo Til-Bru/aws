786807c7ef6807d5b27963d397a8ffed6a53f9ae
Tilmann Bruns <tilmannbruns@Tilmanns-MacBook-Air.local>
2024-11-15 02:04:33 +0800

    New files

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hallo")
+from fastapi import FastAPI
+
+# Create an instance of FastAPI
+app = FastAPI()
+
+# Define a simple route
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
